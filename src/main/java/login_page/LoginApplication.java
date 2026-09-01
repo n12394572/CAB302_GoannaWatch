20198e769f1f0d5cb5a4fad72375f077fdf30f8c
Wyatt Moore <wyattamoore07@outlook.com>
2026-09-01 13:30:50 +1000

    Added comment on previous commit

--- src/main/java/login_page/LoginApplication.java
+++ src/main/java/login_page/LoginApplication.java
@@ -7,7 +7,7 @@ import javafx.fxml.FXMLLoader;
 import java.io.IOException;
 
 public class LoginApplication extends Application {
-
+    // Defines running the Login Page window and the window's various qualities
     @Override
     public void start(Stage stage) throws IOException{
         FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("login.fxml"));
