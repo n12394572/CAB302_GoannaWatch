b3bac3487ece62cd261c38cf5c7a1764f7fea6c3
Harry <n12394572@qut.edu.au>
2026-09-08 17:58:21 +1000

    Updated Login Controller functionality

--- src/main/java/account/controller/LoginController.java
+++ src/main/java/account/controller/LoginController.java
@@ -1,13 +1,21 @@
 package account.controller;
 
 import account.model.IAccountDAO;
+import account.model.Account;
+import account.model.MockAccountDAO;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.util.List;
+import java.util.Optional;
 
 public class LoginController {
-    // private final IAccountDAO accountDAO;
+    private final IAccountDAO accountDAO;
     private boolean rememberMe = false;
 
     @FXML
@@ -28,6 +36,10 @@ public class LoginController {
     @FXML
     private Button nextButton;
 
+    public LoginController() {
+        accountDAO = new MockAccountDAO();
+    }
+
     @FXML
     protected void onRememberMeCheck() {
         rememberMe = rememberMeCheck.isSelected();
@@ -40,7 +52,42 @@ public class LoginController {
 
     @FXML
     private void onNextButtonClick() {
-        System.out.println("Next clicked");
+        String email = emailTextField.getText() == null ? "" : emailTextField.getText().trim();
+        String password = passwordTextField.getText() == null ? "" : passwordTextField.getText().trim();
+
+        if (email.isBlank() || password.isBlank()) {
+            showAlert(Alert.AlertType.WARNING, "Missing information", "Please enter both email or password.");
+        }
+
+        Optional<Account> matchedAccount = findAccountByEmail(email);
+
+        if (matchedAccount.isEmpty() || !matchedAccount.get().getPassword().equals(password)){
+            showAlert(Alert.AlertType.ERROR, "Login failed", "Incorrect email or password.");
+            return;
+        }
+
+        Account account = matchedAccount.get();
+        System.out.println("Logging in as " + account.getFullName() + (rememberMe ? "(remember me enabled)" : ""));
+    }
+
+    private Optional<Account> findAccountByEmail(String email) {
+        List<Account> accounts = accountDAO.getAllAccounts();
+        return accounts.stream()
+                .filter(a -> a.getEmail().equalsIgnoreCase(email))
+                .findFirst();
+    }
+
+    private void showAlert(Alert.AlertType type, String title, String message) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    private void closeWindow(Button anyButtonOnScene) {
+        Stage stage = (Stage) anyButtonOnScene.getScene().getWindow();
+        stage.close();
     }
 }
 
