82d19621a52da629a1816e83edb8514092245ef6
KaydenLaMotte <n12361461@qut.edu.au>
2026-09-11 16:06:39 +1000

    Redesign sign up page to match UI sketches

--- src/main/java/account/controller/SignupController.java
+++ src/main/java/account/controller/SignupController.java
@@ -1,9 +1,19 @@
 package account.controller;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 
 public class SignupController extends Application {
+
+
+    @FXML
+    private ChoiceBox<String> userType = new ChoiceBox<>();
 
     public static void main(String[] args) {
         launch(args);
@@ -12,5 +22,12 @@ public class SignupController extends Application {
     @Override
     public void start(Stage primaryStage) {
 
+    }
+
+    public void userTypeChoice() {
+        userType.getItems().setAll("regular user", "expert user");
+    }
+
+    public void loginRedirectClick() {
     }
 }
